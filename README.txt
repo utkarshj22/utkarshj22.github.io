f2821ed4fd71679555253128a2d180fa410479bf
utkarshj22 <12637569+utkarshj22@users.noreply.github.com>
2021-02-16 22:07:10 +0530

    Add files via upload
    Initial portfolio commit

---
+++ README.txt
@@ -0,0 +1,30 @@
+@Portfolio By Saurabh Talreja
+Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+
+A super simple responsive portfolio template. It's only a single page because hey, sometimes
+you don't need a half dozen pages to make your point.
+
+Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
+you can use for pretty much whatever.
+
+(* = Not included)
+
+Feedback, bug reports, and comments are not only welcome, but strongly encouraged :)
+
+ saurabhtalreja19@gmail.com
+
+
+
+
+Credits:
+
+	Demo Images:
+		Unsplash (unsplash.com)
+
+	Icons:
+		Font Awesome (fontawesome.io)
+
+	Other:
+		jQuery (jquery.com)
+		Responsive Tools (github.com/ajlkn/responsive-tools)
---
+++ assets/js/main.js
@@ -0,0 +1,193 @@
+(function ($) {
+  //contact form functions
+  $(function () {
+    $("#emailError").hide();
+    $("#subjectError").hide();
+    $("#form-submit-message").hide();
+    var error_email = false;
+
+    //check email error function
+    function check_email() {
+      var pattern = new RegExp(/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i);
+      if (pattern.test($("#email").val())) {
+        $("#emailError").hide();
+      } else {
+        $("#emailError").html("Please enter a valid email id.");
+        $("#emailError").show();
+        error_email = true;
+      }
+    }
+
+    // check subject error fucntion
+    function check_subject() {
+      if ($("#subject").val().length > 50) {
+        $("#subjectError").hide();
+        return false;
+      }
+      if (
+        $("#subject").focusout(function () {
+          $("#subjectError").hide();
+        })
+      );
+      $("#subjectError").html(
+        "Remaining characters : " + (100 - $("#subject").val().length)
+      );
+      $("#subjectError").show();
+    }
+
+    //clear form function
+    function clear_form() {
+      $("#name").val("");
+      $("#email").val("");
+      $("#subject").val("");
+      $("#message").val("");
+    }
+
+    //control messages on focus
+    $("#name").focusin(function () {
+      $("#form-submit-error").hide();
+      $("#form-submit-message").hide();
+    });
+    $("#email").focusin(function () {
+      $("#form-submit-error").hide();
+
+      $("#form-submit-message").hide();
+    });
+    $("#subject").focusin(function () {
+      $("#form-submit-error").hide();
+      $("#form-submit-message").hide();
+    });
+    $("#message").focusin(function () {
+      $("#form-submit-error").hide();
+      $("#form-submit-message").hide();
+    });
+
+    $("#subject").keypress(function () {
+      check_subject();
+    });
+
+    $("#email").focusout(function () {
+      check_email();
+    });
+
+    //netlify fprm submission
+    $("#contact-form").submit(function (e) {
+      e.preventDefault();
+      error_email = false;
+
+      //validating email before submission
+      check_email();
+
+      var emailInput = $("#email").val();
+      var subjectInput = $("#subject").val();
+      var textMsgInput = $("#message").val();
+      var nameInput = $("#name").val();
+
+      if (
+        emailInput &&
+        subjectInput &&
+        textMsgInput &&
+        nameInput &&
+        error_email == false
+      ) {
+        var $form = $(this);
+        $.post($form.attr("action"), $form.serialize()).then(function () {
+          $("#form-submit-message").html(
+            "I have recevied your message! Thanks for writing."
+          );
+          $("#form-submit-message").show();
+          clear_form();
+        });
+      } else {
+        clear_form();
+        $("#form-submit-error").html(
+          "Please fill in the details before submitting."
+        );
+        $("#form-submit-error").show();
+      }
+    });
+
+    $("#clear-form").click(function () {
+      $("#name").val("");
+      $("#email").val("");
+      $("#subject").val("");
+      $("#message").val("");
+      $("#form-submit-error").hide();
+      $("#form-submit-message").hide();
+      $("#emailError").hide();
+      $("#subjectError").hide();
+    });
+  });
+  var $window = $(window),
+    $body = $("body"),
+    $nav = $("#nav");
+
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: [null, "736px"],
+  });
+
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+
+    $(".post-wrapper").slick({
+      slidesToShow: 2,
+      //add scrooll
+      autoplay: true,
+      autoplaySpeed: 2000,
+      nextArrow: $(".next"),
+      prevArrow: $(".prev"),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 2000,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true,
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
+    });
+  });
+
+  // Scrolly.
+  $("#nav a, .scrolly").scrolly({
+    speed: 1000,
+    offset: function () {
+      return $nav.height();
+    },
+  });
+})(jQuery);
